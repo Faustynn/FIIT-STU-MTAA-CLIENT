5aee1cd805256a13a36e0e15b960d1a53bf4108a
Gh0st3r0k <gh0st3r0k@gmail.com>
2025-03-18 18:21:51 +0100

    first commit

--- screens/details.tsx
+++ screens/details.tsx
@@ -1,6 +1,7 @@
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { ScreenContent } from 'components/ScreenContent';
 import { StyleSheet, View } from 'react-native';
+import { Button, H1, YStack } from 'tamagui'; // Импорт Tamagui-компонентов
 
 import { RootStackParamList } from '../navigation';
 
@@ -10,11 +11,25 @@ export default function Details() {
   const router = useRoute<DetailsScreenRouteProp>();
 
   return (
-    <View style={styles.container}>
-      <ScreenContent
-        path="screens/details.tsx"
-        title={`Showing details for user ${router.params.name}`}
-      />
+    // <View style={styles.container}>
+    //   <ScreenContent
+    //     path="screens/details.tsx"
+    //     title={`Риздец, нахуй блять ${router.params.name}`}
+    //   />
+    // </View>
+    <View style={{ flex: 1, padding: 24 }}>
+      <YStack space>
+        <H1 color="blue">Профиль пользователя</H1>
+        <H1>{router.params.name}</H1>
+
+        <Button
+          size="$4"
+          backgroundColor="purple"
+          color="white"
+          onPress={() => alert('Кнопка нажата!')}>
+          Нажми меня
+        </Button>
+      </YStack>
     </View>
   );
 }
